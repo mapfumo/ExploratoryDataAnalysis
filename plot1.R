3b18f0ea85f26e1aac02582614f3c300ff593b0a
Antony Mapfumo <mapfumo@gmail.com>
2015-03-07 13:04:49 +1000

    Final submission

--- plot1.R
+++ plot1.R
@@ -1,3 +1,7 @@
+#### Plot 1, Project 2, Coursera - Exploratory Data Analysis
+#### Date: 7 March 2015
+#### Author: Antony Mapfumo
+
 setwd("/home/tony/ExploratoryDataAnalysis")
 NEI <- readRDS("~/tmp/summarySCC_PM25.rds")
 SCC <- readRDS("~/tmp/Source_Classification_Code.rds")
@@ -12,7 +16,7 @@ bmore.emissions.aggr <- aggregate(Emissions ~ year, data=bmore.emissions, FUN=su
 total.emissions <- aggregate(Emissions ~ year, NEI, sum)
 
 png('plot1.png')
-barplot(height=total.emissions$Emissions, names.arg=total.emissions$year,
-        xlab="years", ylab=expression('total PM'[2]*' emission'),
+barplot(height=total.emissions$Emissions, names.arg=total.emissions$year, xlab="years", 
+        ylab=expression('total PM'[2]*' emission'),
         main=expression('Total PM'[2]*' emissions at various years'))
 dev.off()
